02804eacb06f6d89a850bef0b5ea7bb7d3b9a216
Marialaina <pmarialaina@gmail.com>
2020-11-03 14:31:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+// console.log(3 * 5);
+// const divide = 10 / 2;
+// const power = Math.pow(3, 2);
+// const squareRt = Math.sqrt(4);
+
+
+// const rand = Math.round(Math.random() * 52);
+// console.log(rand);
+
+//String
+//"abc", "casey", "AHHH"
+
+// const name = "casey";
+// const lname = "harding";
+// console.log("21" + 5);
+// console.log("21"-2);
+// console.log("casey" - 5);
+
+//let us cont
+//these are the same
+// let count = 0;
+// count = count + 1;
+// count += 1;
+// count  ++
+// console.log(count);
+
+//these are the same
+//for subtraction
+// count = 0;
+// count = count - 1;
+// count -= 1;
+// count --;
+
+// console.log(count);
+
+//toString converts a number to a string
+// console.log((515).toString())
+//parsInt converts a string to a number
+// console.log(parseInt('21') + 5);
+
+
+//NOTES ON ARRAYS!!!!
+
+// const userOne = "adrian";
+// const userTwo = "mari";
+// const USerThree = "gabe";
+
+// const users = ['adrian', 'gabe', 'mari'];
+// console.log(users);
+//adds a user at the end of the array
+// users.push('dom');
+// console.log(users[0]);
+//to find length of an array
+// console.log(users.length);
+
+//to get the last element of an array
+
+// console.log(users.length - 1);
+//getting last element of an array
+// console.log(users[3]);
+
+// grades = [32, 67, 91, 4, 78];
+
+//1) where to start the loop
+//2) where to end the loop
+//3) how to update the counter
+// for (let i = 0 ; i  < grades.length ; i++){
+//     console.log(grades[i])
+// }
+
+//i = 0;
+
+//implementing a number to the array
+
+// for (let i = 0 ; i < grades.length ; i++){
+//     console.log(grades[i] + 20);
+// }
+
+// grades = [32, 67, 91, 4, 78];
+
+// grades.forEach(function(elm) {
+//     elm += 20;
+//     console.log(elm)
+// })
+
+// const evens = [2, 4, 6, 8, 10];
+// const odds = [1, 3, 5, 7, 9];
+
+// const outcome = evens.every(function(num) {
+//     return num % 2 === 0;
+// })
+
+// const loosseyOutcome = odds.some(function(num){
+//     return num % 2  === 0;
+// })
+
+// console.log(outcome);
+// console.log(loosseyOutcome);
+
+// const bigEvens = evens.filter(function(num){
+//     return num < 5 
+// })
+
+// console.log(bigEvens);
+
+// const filmnames = ['image1.png', 'image2.png', 'image3.png'];
+
+// console.log(filmnames);
+
+// const q2 = filmnames[0];
+
+// console.log(q2);
+
+// const q3 = filmnames.length;
+
+// console.log(q3);
+
+// console.log(filmnames.length - 1);
+
+// q4 = filmnames[2];
+
+// console.log(q4);
+
+// for (let i = 0 ; i  < grades.length ; i++){
+//     console.log(grades[i])
+// }
+
+// const num = [1, 2, 3, 4];
+
+// for (let i = 0 ; i < num.length ; i++){
+//     newNum = num[i] + 1;
+//     console.log(newNum);
+// }
+
+
+
